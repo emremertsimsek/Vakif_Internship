caab3e37ea3b0c3e5877928b4615f65b14613d63
emremertsimsek <140048361+emremertsimsek@users.noreply.github.com>
2023-08-04 16:50:58 +0300

    Add files via upload

---
+++ Vakif_Internship/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Vakif_Internship
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.lbFiles = new System.Windows.Forms.ListBox();
+            this.lbResult = new System.Windows.Forms.ListBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(270, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kontrol Edilmek İçin Seçilen Dosyalar";
+            // 
+            // btnScan
+            // 
+            this.btnScan.Location = new System.Drawing.Point(592, 316);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(385, 69);
+            this.btnScan.TabIndex = 1;
+            this.btnScan.Text = "2- Seçilen Dosyaları Tara";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(592, 40);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(385, 69);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "1-Taranacak Dosyaları Seçiniz(Tek Seferde Hepsi Seçilmeli)";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // lbFiles
+            // 
+            this.lbFiles.FormattingEnabled = true;
+            this.lbFiles.ItemHeight = 20;
+            this.lbFiles.Location = new System.Drawing.Point(34, 118);
+            this.lbFiles.Name = "lbFiles";
+            this.lbFiles.Size = new System.Drawing.Size(943, 184);
+            this.lbFiles.TabIndex = 3;
+            // 
+            // lbResult
+            // 
+            this.lbResult.FormattingEnabled = true;
+            this.lbResult.ItemHeight = 20;
+            this.lbResult.Location = new System.Drawing.Point(34, 398);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(943, 204);
+            this.lbResult.TabIndex = 4;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(592, 608);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(385, 69);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "3- ListBox\'ları Temizle";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 365);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(581, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Hataların Bulundukları Cs Dosyalarının Yolları ve Bulundukları Satırların Numaral" +
+    "arı";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1011, 689);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.lbFiles);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.btnScan);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.ListBox lbFiles;
+        private System.Windows.Forms.ListBox lbResult;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Vakif_Internship/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+
+namespace Vakif_Internship
+{
+    public partial class Form1 : Form
+    {
+        private List<string> selectedFilePaths = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            //Dosya seçmek için OpenFileDialog oluşturuluyor ve çoklu dosya seçimi etkinleştiriliyor.
+            //Sadece C# dosyalarını (*.cs) seçilebilecek şekilde filtre ekleniyor.
+            OpenFileDialog openFileDialog= new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "C# Files (*.cs)|*.cs";
+
+            //Kullanıcı bir veya daha birden fazla dosya seçtiyse, seçilen dosya yolları lbFiles ListBox'a ekleniyor.
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                selectedFilePaths = openFileDialog.FileNames.ToList();
+                lbFiles.Items.Clear();
+                lbFiles.Items.AddRange(selectedFilePaths.ToArray()); //Seçilen dosya yolları inceleniyor.
+            }
+        }
+
+        //"Tara" butonuna tıklandığında çalışacak olan olay işleyici.
+        private void btnScan_Click(object sender, EventArgs e)
+        {
+            lbResult.Items.Clear();
+            foreach(string filePath in selectedFilePaths) //Seçilen her bir dosya yolu için döngü.
+            {
+                string fileContent = File.ReadAllText(filePath); //Dosya içeriği okunuyor.
+                List<int> lineNumbers = FindBaseExceptionLineNumbers(fileContent); //Hatalı satır numaraları bulunuyor.
+                
+                if(lineNumbers.Count > 0) //Eğer hatalı satır numaraları bulunduysa:
+                {
+
+                    string lineNumbersText = string.Join(", ", myCOnverter(lineNumbers));
+                    lbResult.Items.Add(filePath + " - Line Numbers: " +lineNumbersText);
+                    
+                }
+
+            }
+        }
+
+        
+        private string[] myCOnverter(List<int> myList)
+        {
+            string[] converted = new string[myList.Count];
+            for (int i = 0; i < myList.Count; i++)
+            {
+                converted[i] = myList[i].ToString();
+            }
+            return converted;
+        }
+        
+
+        //BaseException kullanımlarının bulunduğu satır numaralarını döndüren fonksiyon.
+        private List<int> FindBaseExceptionLineNumbers(string content)
+        {
+            List<int> lineNumbers=new List<int>(); //Hatalı satır numaralarını sakalamak için bir liste oluşturuluyor.
+            int lineNumber = 1; //Satır numarası başlangıç değeri.
+            string pattern = @"throw\s+new\s+BaseException\("; //BaseException kullanımı tanımlayan Regex deseni.
+
+            using (StringReader reader = new StringReader(content)) 
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    //Eğer satır içinde BaseException kullanımı bulunuyorsa ve ExceptionTypes içermiyorsa
+                    if(Regex.IsMatch(line, pattern) && !line.Contains("ExceptionTypes"))
+                    {
+                        lineNumbers.Add(lineNumber); //Hatalı satır numarası ekleniyor.
+                    }
+                    lineNumber++; //Sonraki satıra geçiliyor
+                }
+            }
+            return lineNumbers; //Bulunan hatalı satır numaraları döndürülüyor.
+        }
+
+
+        //Temizle butonuna tıklandığında çalışacak olan olay işleyicisi
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lbFiles.Items.Clear(); //Dosya ListBox'ı temizleniyor.
+            lbResult.Items.Clear();
+        }
+    }
+}
